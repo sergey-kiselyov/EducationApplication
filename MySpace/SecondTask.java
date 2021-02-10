53d798898995952de944c023403c513d10e34c43
volodymyrkorniienko <v.a.korniienko@gmail.com>
2021-02-10 11:35:05 +0200

    Java Practice

---
+++ MySpace/SecondTask.java
@@ -0,0 +1,47 @@
+package MySpace;
+
+import java.util.Scanner;
+
+public class SecondTask {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи число от 0 до 10");
+        int number = scanner.nextInt();
+
+        switch (number){
+            case 0:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 10:
+                System.out.println("Ten");
+                break;
+            default:
+                System.out.println("Значение не верно, введите число от 0 до 10");
+        }
+    }
+}
